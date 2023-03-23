f82d301d84a4b992d73f371a24b1b812fb8bee7d
jinxuan.chen <jinxuan.chen@pixocial.com>
2023-03-23 17:06:48 +0800

    fix

--- javascript/exporterwebui.js
+++ javascript/exporterwebui.js
@@ -29,7 +29,7 @@ function attachGalleryListeners1(tab_name) {
   //     gradioApp().getElementById(tab_name+"_exporter_button").click()
   // });
   console.log("tab_name")
-  console.log(tab_name)
+  console.log(gradioApp())
   
   // 创建导出按钮
   const exporterButton = document.createElement("button");
