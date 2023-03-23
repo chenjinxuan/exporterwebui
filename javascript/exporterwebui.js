303905f9858969d2bb8ee53944e150b4123c8282
jinxuan.chen <jinxuan.chen@pixocial.com>
2023-03-23 16:31:33 +0800

    fix

--- javascript/exporterwebui.js
+++ javascript/exporterwebui.js
@@ -24,10 +24,10 @@ let modalObserver1 = new MutationObserver(function(mutations) {
 function attachGalleryListeners1(tab_name) {
   gallery = gradioApp().querySelector('#'+tab_name+'_gallery')
   gallery?.addEventListener('click', () => gradioApp().getElementById(tab_name+"_exporter_button").click());
-  gallery?.addEventListener('keydown', (e) => {
-    if (e.keyCode == 37 || e.keyCode == 39) // left or right arrow
-      gradioApp().getElementById(tab_name+"_exporter_button").click()
-  });
+  // gallery?.addEventListener('keydown', (e) => {
+  //   if (e.keyCode == 37 || e.keyCode == 39) // left or right arrow
+  //     gradioApp().getElementById(tab_name+"_exporter_button").click()
+  // });
   console.log("tab_name")
   console.log(tab_name)
   
@@ -36,24 +36,29 @@ function attachGalleryListeners1(tab_name) {
   exporterButton.innerHTML = "Export";
   exporterButton.id = tab_name+"_exporter_button";
   exporterButton.style.display = "none";
-  exporterButton.addEventListener("click", () => {
-    const data = gradioInterface.inputs;
-    exportData(JSON.stringify(data));
-  });
+exporterButton.onclick = function() {
+  let formValues = gradioApp().getInputValues();
+  let dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(formValues));
+  let dlAnchorElem = document.createElement('a');
+  dlAnchorElem.setAttribute("href", dataStr);
+  dlAnchorElem.setAttribute("download", "form-data.json");
+  dlAnchorElem.click();
+}
   exporterButton.className = "gr-button gr-button-lg gr-button-secondary";
   exporterButton.style = `padding-left: 0.1em; padding-right: 0em; margin: 0.1em 0;max-height: 2em; max-width: 6em`;
 
   // 创建新的div
-  const actionsColumn = document.createElement("div");
-  actionsColumn.id = `${tab_name}_actions_column`;
-  actionsColumn.className = "flex-shrink-0 pl-3 pr-3 pt-2 pb-2 text-right overflow-hidden";
-  actionsColumn.appendChild(exporterButton);
+  // const actionsColumn = document.createElement("div");
+  // actionsColumn.id = `${tab_name}_actions_column`;
+  // actionsColumn.className = "flex-shrink-0 pl-3 pr-3 pt-2 pb-2 text-right overflow-hidden";
+  // actionsColumn.appendChild(exporterButton);
 
-  // 将新创建的div添加到页面上
-  const parent = gradioApp().querySelector('#'+tab_name+'_gallery')
-  parent.insertBefore(actionsColumn, parent.firstChild);
-
+  // // 将新创建的div添加到页面上
+  // const parent = gradioApp().querySelector('#'+tab_name+'_gallery')
+  // parent.insertBefore(actionsColumn, parent.firstChild);
+gallery.appendChild(exporterButton);
   return gallery;
+
 }
 
 function exportData(data) {
