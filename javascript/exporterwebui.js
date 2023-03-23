12e410fe987e4ed027cad1d83c94a7c738a6b8a8
jinxuan.chen <jinxuan.chen@pixocial.com>
2023-03-23 11:44:32 +0800

    fix

--- javascript/exporterwebui.js
+++ javascript/exporterwebui.js
@@ -1,6 +1,48 @@
 let txt2img_gallery1, img2img_gallery1, modal1 = undefined;
 
+
+
+
+function createButton(id, innerHTML, onClick) {
+  const button = document.createElement("button");
+  button.id = id;
+  button.type = "button";
+  button.innerHTML = innerHTML;
+  button.className = "gr-button gr-button-lg gr-button-secondary";
+  button.style = `padding-left: 0.1em; padding-right: 0em; margin: 0.1em 0;max-height: 2em; max-width: 6em`;
+  button.addEventListener("click", onClick);
+  return button;
+}
+
 onUiUpdate(function(){
+  const actionsColumn = gradioApp().querySelector("#txt2img_actions_column");
+  const nai2local = gradioApp().querySelector("#nai2local");
+
+ console.log("==========");
+  if (!actionsColumn || nai2local) return;
+ console.log("==========1111");
+ 
+
+  const nai2LocalArea = document.createElement("div");
+  nai2LocalArea.id = "nai2local";
+  nai2LocalArea.className = "overflow-hidden flex col gap-4";
+  nai2LocalArea.style = "padding: 0.4em 0em";
+
+  const convertBtn = createButton(
+    "exporter",
+    "exporter",
+    function(){
+    const data = gradioInterface.inputs;
+    exportData(JSON.stringify(data));
+  }
+  );
+
+  nai2LocalArea.appendChild(convertBtn);
+
+  actionsColumn.append(nai2LocalArea);
+
+
+
   if (!txt2img_gallery1) {
     txt2img_gallery1 = attachGalleryListeners1("txt2img")
   }
@@ -30,26 +72,26 @@ function attachGalleryListeners1(tab_name) {
   });
   console.log("=========="${tab_name});
   // 创建导出按钮
-  const exporterButton = document.createElement("button");
-  exporterButton.innerHTML = "Export";
-  exporterButton.id = tab_name+"_exporter_button";
-  exporterButton.style.display = "none";
-  exporterButton.addEventListener("click", () => {
-    const data = gradioInterface.inputs;
-    exportData(JSON.stringify(data));
-  });
+  // const exporterButton = document.createElement("button");
+  // exporterButton.innerHTML = "Export";
+  // exporterButton.id = tab_name+"_exporter_button";
+  // exporterButton.style.display = "none";
+  // exporterButton.addEventListener("click", () => {
+  //   const data = gradioInterface.inputs;
+  //   exportData(JSON.stringify(data));
+  // });
   
-  exporterButton.className = "gr-button gr-button-lg gr-button-secondary";
-  exporterButton.style = `padding-left: 0.1em; padding-right: 0em; margin: 0.1em 0;max-height: 2em; max-width: 6em`;
+  // exporterButton.className = "gr-button gr-button-lg gr-button-secondary";
+  // exporterButton.style = `padding-left: 0.1em; padding-right: 0em; margin: 0.1em 0;max-height: 2em; max-width: 6em`;
 
-  const nai2LocalArea = document.createElement("div");
-  nai2LocalArea.id = "nai2local";
-  nai2LocalArea.className = "overflow-hidden flex col gap-4";
-  nai2LocalArea.style = "padding: 0.4em 0em";
-  nai2LocalArea.appendChild(exporterButton);
+  // const nai2LocalArea = document.createElement("div");
+  // nai2LocalArea.id = "nai2local";
+  // nai2LocalArea.className = "overflow-hidden flex col gap-4";
+  // nai2LocalArea.style = "padding: 0.4em 0em";
+  // nai2LocalArea.appendChild(exporterButton);
 
 
-  const actionsColumn = gradioApp().querySelector(`#${tab_name}_actions_column`);
+  // const actionsColumn = gradioApp().querySelector(`#${tab_name}_actions_column`);
 
   // 添加导出按钮
   // const toolbar = gradioApp().querySelector(`#${tab_name}_generation_info_toolbar`);
